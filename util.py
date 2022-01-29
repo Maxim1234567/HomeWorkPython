99984e13381113508de82ff63dd9f8669b6ac450
Maxim1234567 <anti123456.antipov@yandex.ru>
2022-01-29 20:49:04 +0300

    HomeWork 4 lesson 4

---
+++ util.py
@@ -0,0 +1,17 @@
+def multiplication(a: int, b: int):
+    """
+    Функция умножения двух целых чисел
+    :param a: число 1
+    :param b: число 2
+    :return: произведение
+    """
+    return a * b
+
+def add(a: list):
+    """
+    Увеличивает значение аргумента на 1
+    :param a: аргумент
+    :return: результат
+    """
+    a[0] += 1
+    return a
